135c7f7489fa0584d2449e16ec03d2264d9c5438
admin <paulcommons@gmail.com>
2012-06-28 21:08:07 -0500

    working stripped down file of activity.py

--- PrinterBot.activity/PrinterBotActivity.py
+++ PrinterBot.activity/PrinterBotActivity.py
@@ -1,19 +1,3 @@
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-
-# The sharing bits have been taken from ReadEtexts
-
 from __future__ import division
 
 from sugar.activity import activity
@@ -39,75 +23,64 @@ from sugar.datastore import datastore
 import telepathy
 import dbus
 
+import ImageView
 import ProgressDialog
-import Printcore
+import pronsole
 
 _logger = logging.getLogger('PrinterBot-activity')
 
+import os, gettext, Queue, re
 
+if os.path.exists('/usr/share/pronterface/locale'):
+    gettext.install('pronterface', '/usr/share/pronterface/locale', unicode=1)
+else:
+    gettext.install('pronterface', './locale', unicode=1)
+
+import printcore, sys, glob, time, threading, traceback, traceback, cStringIO, subprocess
+try:
+    os.chdir(os.path.split(__file__)[0])
+except:
+    pass
+StringIO=cStringIO
+
+thread=threading.Thread
+
+"""
 class PrinterBotHTTPRequestHandler(network.ChunkedGlibHTTPRequestHandler):
-    """HTTP Request Handler for transferring document while collaborating.
-
-    RequestHandler class that integrates with Glib mainloop. It writes
-    the specified file to the client in chunks, returning control to the
-    mainloop between chunks.
-
-    """
-
     def translate_path(self, path):
-        """Return the filepath to the shared document."""
         return self.server.filepath
 
 
 class PrinterBotHTTPServer(network.GlibTCPServer):
-    """HTTP Server for transferring document while collaborating."""
-
     def __init__(self, server_address, filepath):
-        """Set up the GlibTCPServer with the PrinterBotHTTPRequestHandler.
-
-        filepath -- path to shared document to be served.
-        """
         self.filepath = filepath
         network.GlibTCPServer.__init__(self, server_address,
                                        PrinterBotHTTPRequestHandler)
 
 
 class PrinterBotURLDownloader(network.GlibURLDownloader):
-    """URLDownloader that provides content-length and content-type."""
 
     def get_content_length(self):
-        """Return the content-length of the download."""
         if self._info is not None:
             return int(self._info.headers.get('Content-Length'))
 
     def get_content_type(self):
-        """Return the content-type of the download."""
         if self._info is not None:
             return self._info.headers.get('Content-type')
         return None
 
 PrinterBot_STREAM_SERVICE = 'PrinterBot-activity-http'
+"""
+class PrinterBotActivity(activity.Activity, pronsole.pronsole):
 
-
-
-class PrinterBotActivity(activity.Activity):
-
-    def __init__(self, handle):
+    def __init__(self, handle, filename=None):
         activity.Activity.__init__(self, handle)
-
-        self._object_id = handle.object_id
-
-        self._fileserver = None
-        self._fileserver_tube_id = None
-
-        self.progressdialog = None
+	pronsole.pronsole.__init__(self)
 
         toolbar_box = ToolbarBox()
         self._add_toolbar_buttons(toolbar_box)
         self.set_toolbar_box(toolbar_box)
         toolbar_box.show()
-        
-        self.set_canvas(self.startprint_button)
 
     def handle_view_source(self):
         raise NotImplementedError
@@ -117,17 +90,13 @@ class PrinterBotActivity(activity.Activity):
         toolbar_box.toolbar.insert(activity_button, 0)
         activity_button.show()
 
-        startprint_button = ToolButton('startprint')
-        startprint_button.set_tooltip(_('Start Print'))
-        startprint_button.connect('clicked', self.startprint)
-        toolbar_box.toolbar.insert(startprint_button, -1)
-        startprint_button.show()
+	connect_button = ToolButton('connect')
+	connect_button.set_tooltip(_('Connect'))
+	connect_button.connect('clicked',self.connection)
+	toolbar_box.toolbar.insert(connect_button, -1)
+	connect_button.show()
 
-        spacer = gtk.SeparatorToolItem()
-        spacer.props.draw = False
-        toolbar_box.toolbar.insert(spacer, -1)
-        spacer.show()
-
+        self.serialport = self.scanserial()
 
         separator = gtk.SeparatorToolItem()
         separator.props.draw = False
@@ -139,5 +108,38 @@ class PrinterBotActivity(activity.Activity):
         toolbar_box.toolbar.insert(stop_button, -1)
         stop_button.show()
 
-    def startprint(self, widget, data=None):
-        logging.info('Printing')
+    def scanserial(self):
+        #scan for available ports. return a list of device names.
+        baselist=[]
+        if os.name=="nt":
+            try:
+                key=_winreg.OpenKey(_winreg.HKEY_LOCAL_MACHINE,"HARDWARE\\DEVICEMAP\\SERIALCOMM")
+                i=0
+                while(1):
+                    baselist+=[_winreg.EnumValue(key,i)[1]]
+                    i+=1
+            except:
+                pass
+        return baselist+glob.glob('/dev/ttyUSB*') + glob.glob('/dev/ttyACM*') +glob.glob("/dev/tty.*")+glob.glob("/dev/cu.*")+glob.glob("/dev/rfcomm*")
+
+    def connection(self,event):
+        print _("Connecting...")
+        port=None
+        try:
+            port=self.scanserial()[0]
+        except:
+            pass
+        if self.serialport.GetValue()!="":
+            port=str(self.serialport.GetValue())
+        baud=115200
+        try:
+            baud=int(self.baud.GetValue())
+        except:
+            pass
+        self.p.connection(port,baud)
+        #self.statuscheck=True
+        if port != self.settings.port:
+            self.set("port",port)
+        if baud != self.settings.baudrate:
+            self.set("baudrate",str(baud))
+        threading.Thread(target=self.statuschecker).start()
